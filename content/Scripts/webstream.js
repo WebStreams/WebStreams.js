0376591fb10be2ff27e26c91be526a7ffcd12312
Reuben Bond <reuben.bond@outlook.com>
2014-06-06 08:30:49 +1000

    Improved URL handling and documentation.

--- content/Scripts/webstream.js
+++ content/Scripts/webstream.js
@@ -1,16 +1,36 @@
-;(function(global) {
-    var init = function(context) {
-        context.WebStream = function(path, params, inputs) {
-            return Rx.Observable.create(function(observer) {
+; (function (global) {
+    var init = function (context) {
+        /**
+        * Connect to the stream specified by path, returning an observable.
+        * 
+        * @method WebStream
+        * @param {String} path The path of the WebSocket to connect to.
+        *   Either an absolute path ("/stock/ticker") or a URI.
+        * @param {Object} queryParams The query parameters to append to the path.
+        * @param {Object} inputStreams The collection of observables to pipe into
+        *   the stream. Note that keys must match those specified on the service.
+        * @return {Boolean} Returns an Rx.Observable which will connect to the 
+        *   specified path when subscribed to, emitting all received messages.
+        */
+        context.WebStream = function (path, queryParams, inputStreams) {
+            return Rx.Observable.create(function (observer) {
                 var self = {};
-                self.params = params || {};
+                self.params = queryParams || {};
                 self.subscriptions = [];
-                self.inputs = inputs || {};
+                self.inputs = inputStreams || {};
+
+                // Convert path into a URL if it isn't already.
+                if (/^\w+\:\/\//i.test(path)) {
+                    self.url = path;
+                } else {
+                    // Use the current origin, replacing http with ws and https with wss.
+                    var origin = window.location.origin;
+                    self.url = origin.replace(/^http(\w)?\:\/\//i, 'ws$1://') + path;
+                }
 
                 // Add all parameters to the URL query string.
-                self.url = window.location.origin.replace('http://', 'ws://') + path;
-                if (params) {
-                    self.url += '?' + toQueryString(params);
+                if (self.params) {
+                    self.url += '?' + toQueryString(self.params);
 
                     // Encode the given object as a query string.
                     function toQueryString(object) {
@@ -23,7 +43,7 @@
                         return parts.join('&')
                     }
                 }
-                
+
                 // Connect to the uri.
                 self.socket = new WebSocket(self.url);
 
@@ -57,35 +77,35 @@
                 self.socket.onmessage = function (message) {
                     if (message.data.length > 0) {
                         switch (message.data[0]) {
-                        case 'n':
-                            // Propagate 'Next' event.
-                            observer.onNext(JSON.parse(message.data.slice(1)));
-                            break;
-                        case 'e':
-                            // Propagate 'Error' event & dispose.
-                            observer.onError(JSON.parse(message.data.slice(1)));
-                            self.dispose();
-                            break;
-                        case 'c':
-                            // Propagate 'Completed' event & dispose.
-                            observer.onCompleted();
-                            self.dispose();
-                            break;
+                            case 'n':
+                                // Propagate 'Next' event.
+                                observer.onNext(JSON.parse(message.data.slice(1)));
+                                break;
+                            case 'e':
+                                // Propagate 'Error' event & dispose.
+                                observer.onError(JSON.parse(message.data.slice(1)));
+                                self.dispose();
+                                break;
+                            case 'c':
+                                // Propagate 'Completed' event & dispose.
+                                observer.onCompleted();
+                                self.dispose();
+                                break;
                         }
                     }
                 };
 
                 // Subscribe to each input, piping inputs to socket.
                 for (var i in self.inputs) {
-                    self.subscriptions.push(self.inputs[i].subscribe(function(next) {
-                            // Send a 'Next' event.
-                            self.socket.send('n' + i + '.' + JSON.stringify(next));
-                        },
-                        function(error) {
+                    self.subscriptions.push(self.inputs[i].subscribe(function (next) {
+                        // Send a 'Next' event.
+                        self.socket.send('n' + i + '.' + JSON.stringify(next));
+                    },
+                        function (error) {
                             // Send an 'Error' event.
                             self.socket.send('e' + i + '.' + JSON.stringify(error));
                         },
-                        function() {
+                        function () {
                             // Send a 'Completed' event.
                             self.socket.send('c' + i);
                         }));
@@ -98,7 +118,7 @@
     };
 
     if (typeof define === 'function' && define.amd) {
-        define(['rx'], function(){
+        define(['rx'], function () {
             return init({});
         })
     } else {
